39fdfe975ce6592735d4300721f5e13f9507f3ed
marcelo <marckcm@gmail.com>
2025-08-03 01:09:27 -0300

    conserto inconsistencias

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import TaskPage from "./pages/taskPage.jsx";
-
-
+import TaskPage from "./pages/TaskPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
   },
   {
     path: "/task",
-    element: <TaskPage/>,
+    element: <TaskPage />,
   },
 ]);
 
